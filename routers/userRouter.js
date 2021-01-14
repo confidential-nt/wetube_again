df5939aa4f9e1613542c27a9af3ccf39c2352468
confidential-nt <youyoy689@gmail.com>
2021-01-14 10:04:30 +0900

    make some features

---
+++ routers/userRouter.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  changePassword,
+  editProfile,
+  userDetail,
+  users,
+} from "../controllers/userController.js";
+import routes from "../routes.js";
+
+const userRouter = express.Router();
+
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
+
+export default userRouter;

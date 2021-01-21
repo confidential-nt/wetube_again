35c600af21e17459f25de65bc0545c2feffd6fed
confidential-nt <youyoy689@gmail.com>
2021-01-21 21:16:57 +0900

    review complete 1/21

--- app.js
+++ app.js
@@ -7,14 +7,24 @@ import globalRouter from "./routers/globalRouter.js";
 import userRouter from "./routers/userRouter.js";
 import videoRouter from "./routers/videoRouter.js";
 import routes from "./routes.js";
+import { localsMiddleware } from "./middlewares.js";
 const app = express();
 
+app.use(helmet());
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy",
+    "script-src 'self' https://archive.org"
+  );
+  return next();
+});
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);

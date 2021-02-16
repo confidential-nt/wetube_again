044a52deb371f6a1890a83f583ff3edfd8b11401
confidential-nt <youyoy689@gmail.com>
2021-02-17 03:20:26 +0900

    made model

--- init.js
+++ init.js
@@ -1,6 +1,11 @@
+import "./db";
 import app from "./app.js";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+import "./models/Comment";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
